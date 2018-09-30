9748c680bde9eaf1659fa1ad2b7af2892c4701f4
Arram Mandel <airum82@gmail.com>
2018-09-30 13:03:46 -0600

    Added fetch calls to set state with userInfo

--- app/index.js
+++ app/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userInfo: {}
+    }
+  }
+
+  retrievePersonInfo(id) {
+    fetch(`https://api.rach.io/1/public/person/${id}`, {
+      headers: {
+        'Authorization': 'Bearer 76980330-8f0b-4659-a341-527364acf134',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(result => this.setState({ userInfo: result }))
+    .then(error => console.log(error))
+  }
+
+  componentDidMount() {
+    console.log('fire')
+    fetch('https://api.rach.io/1/public/person/info',{
+      headers: { 
+        'Authorization': 'Bearer 76980330-8f0b-4659-a341-527364acf134',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(result => this.retrievePersonInfo(result.id))
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
@@ -16,3 +48,5 @@ ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
+
+const apiKey = '76980330-8f0b-4659-a341-527364acf134';
