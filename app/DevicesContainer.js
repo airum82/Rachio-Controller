cb8b885eac236d852c95821ed1d79ad8e6e16cce
Arram Mandel <airum82@gmail.com>
2018-10-01 17:04:20 -0600

    Add background image and various styling for main page

--- app/DevicesContainer.js
+++ app/DevicesContainer.js
@@ -7,10 +7,10 @@ const DevicesContainer = ({ devices }) => {
     <div className="devices-container">
       { devices.map((device, index) => {
           return (
-            <div className="device-link" key={index}>
+            <div className="devices" key={index}>
               <Route exact path='/' render={() => {
                 return (
-                  <NavLink to={`/${device.id}`}>
+                  <NavLink to={`/${device.id}`} className="device-link">
                     {device.name}
                   </NavLink>
                 )
--- app/index.js
+++ app/index.js
@@ -4,7 +4,8 @@ import { apiKey } from '../APIkey';
 import sprinklers from '../sprinklers.gif'
 import DevicesContainer from './DevicesContainer';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './index.css'
+import './index.css';
+import 'normalize.css';
 
 class App extends Component {
   constructor() {
@@ -45,12 +46,12 @@ class App extends Component {
       <div className="main">
         { this.state.isLoading ?
           <div className="loading">
-          <h1>Retrieving Devices Now</h1>
+          <h1 className="title">Retrieving Devices Now</h1>
           <img src={sprinklers} alt="moving sprinklers to watch while we fetch your content" />
           </div> 
           :
           <div className="main-display">
-            <h1>Hello Arram!</h1>
+            <h1 className="title">Hello Arram!</h1>
             <DevicesContainer devices={devices} />
           </div >
       }
